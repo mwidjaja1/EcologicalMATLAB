61120a3b300c5f696e92b4c667b3a68503010fed
Matthew Widjaja <Matthew@MBRetina.local>
2014-06-28 11:42:56 -0400

    Version 2 - Basic Discovery
    This portion of the code was mainly modified to identify the optimal
    values for the testing of this model.

--- func.m
+++ func.m
@@ -1,51 +1,49 @@
 %% Matthew Widjaja.
 % Research Function Set.
-
-% Instructions: Run these equations from program.m
+% Instructions: Run program_v2.m to utilize these equations
 
 function dy = func(t,y)
 global fixEqu 
+global maxNode
+global constant
+global newAlpha
 
 %% Initial Variables 
 % This determines the characteristics of each organism
 
-growthRate = [1; 2; 2; 1; 1; 2; 2; 1; 1; 2]; 
-carryCap = [15; 15; 15; 15; 15; 15; 15; 15; 15; 15];  
-compEffect = [0, 0.30, 0.45, 0.60, 0.75, 0.90, 1.05, 1.30, 1.45, 0.30;... 
-    0.75, 0, 0.90, 1.05, 1.30, 1.45, 0.30, 0.45, 0.60, 0.75;...
-    1.30, 1.45, 0, 0.30, 0.45, 0.60, 0.75, 0.90, 1.05, 1.30;...
-    0.45, 0.60, 0.75, 0, 0.90, 1.05, 1.30, 1.45, 0.30, 0.45;...
-    0.90, 1.05, 1.30, 1.45, 0, 0.30, 0.45, 0.60, 0.75, 0.90;...
-    1.45, 0.30, 0.45, 0.60, 0.75, 0, 0.90, 1.05, 1.30, 1.45;...
-    0.60, 0.75, 0.90, 1.05, 1.30, 1.45, 0, 0.30, 0.45, 0.60;...
-    1.05, 1.30, 1.45, 0.30, 0.45, 0.60, 0.75, 0, 0.90, 1.05;...
-    0.30, 0.45, 0.60, 0.75, 0.90, 1.05, 1.30, 1.45, 0, 0.30;...
-    0.75, 0.90, 1.05, 1.30, 1.45, 0.30, 0.45, 0.60, 0.75, 0];  
-compEffect = compEffect./1.5;
-a = [0; 0; 0; 0; 0; 0; 0; 0; 0; 0];   % Summation Alpha-Matrix
+steadyValueP = [0.0060, 0.0026, 0.0065, 0.0069, 0.0075, 0.0045, 0.0008, 0.0023]; 
+oldAlpha = [-3.0289, -0.3701, -0.717, -0.448, 0, 0, 0, -0.21633505016943;...
+    -2.0538, -1.43, -0.8654, -0.7504, 0, 0, 0, -0.193866116564711;...
+    -0.0321, -0.3477, -1.0458, -1.3652, 0, 0, 0, -0.0398132096260681;...
+    -0.7317, -0.1804, -0.9557, -1.7452, 0, 0 ,0 ,-0.228368899861282;...
+    0, 0, 0, -0.150931605147117, -1.9238,-0.3892,-0.6804,-0.3006;...
+    0, 0, 0, -0.15553106814765,-0.0058, -1.4085, -0.8097, -0.4383;...
+    0, 0, 0, -0.134422580879091, -0.0613, -0.5699, -0.8766, -0.0381;...
+    0, 0, 0, -0.041028739217153, -0.8943, -0.2053,-0.6153,-1.0412];
 
-%% Calculation of Alpha Values
-% This calculates the Alpha Value of each organism
+%% Constants (p)
+% This calculates the Constants using alpha & steadyValue
 
-for i=1:10  % Creates Alpha-Values for each organism
-    for c=1:10
-        if i == c;  % Skips adding Alpha if it affects itself
-            a(i) = a(i);
-        else % Adds Alpha for other organisms
-            a(i) = a(i) + (compEffect(i,c) * y(c));
-        end
-    end
+constantR = zeros(1,maxNode);		% Generates Constants Matrix
+for(i1 = 1:maxNode)						% Populates Constants Matrix
+	for(i2 = 1:maxNode)
+		newAlpha(i1,i2) = -oldAlpha(i1,i2) / steadyValueP(i1);
+		constantR(i1) = constantR(i1) + (newAlpha(i1,i2) * steadyValueP(i2));
+	end
 end
 
 %% Generation of Equations
 % This creates the most appropriate Diff-Equ for each organism
 
-dy = zeros(10,1);
-for i=1:10
-    if any(i == fixEqu);    % If working on the Fixed Equation, we set it to a no-growth rate
-        dy(i) = 0;
-    else % Otherwise, we create the standard Equation
-        dy(i) = (growthRate(i)* y(i)) * ((1/carryCap(i)) * (carryCap(i) - y(i) - (a(i))));
-    end
-end
+dy = zeros(maxNode,1);	% Generates Equation Matrix
+	for(i1 = 1:maxNode)					   
+		if any(i1 == fixEqu);	% This fixes an Equation's growth if requested in program_v2.m
+			dy(i1) = 0;
+		else	% Otherwise, an Equation is made
+			dy(i1) = constantR(i1) * y(i1);	% ...By multiplying the Constant by Y
+			for(i2 = 1:maxNode)	% ...And subtracting Alpha
+				dy(i1) = dy(i1) - (newAlpha(i1,i2) * y(i1) * y(i2));
+			end
+		end
+	end
 end
--- program.m
+++ program.m
@@ -1,26 +1,35 @@
 %% Matthew Widjaja.
 % Research Program Runner.
-
-% Instructions: This & func.m must be in the same folder &
-% this folder must be the 'Current Folder' in MATLAB.
+% Instructions: This file & func_v2.m must be in the same folder in order to be used.
 
 clear all
 clc
 format short
+
 global fixEqu
+global constant
+global newAlpha
+
+
+%% Node Modification Query
+% This lets the user select how to modify a node, if desired
+fprintf('Methods: 1. AllSKO \n')
+nodeMethod = input('Select a Method (1-5) = ');
+
+
 
 %% Modification of a Node
 % This lets the user change the node to a desired value
 
-ICs = [1 1 1 1 1 1 1 1 1 1];   % Set initial conditions
+ICs = [1 1 1 1 1 1 1 1];		% Set initial conditions
 fix = input('How many nodes should be fixed? (0 -> 10) = ');
 	if fix >= 1
 		fixEqu = zeros(1,fix);
-		for i=1:1:fix
-			fixNode(i) = input('Knockout Node Number = ');   % Node # to modify
-			fixEqu(i) = fixNode(i);   % Saves Node # to a matrix for use in the Function
-			fixValue = input('Knockout with the Value = ');   % How the node should be modified
-			ICs(fixNode(i)) = fixValue;   % Replaces the value of Node # to the declared value
+		for i1=1:1:fix
+			fixNode(i1) = input('Knockout Node Number = ');		% Node # to modify
+			fixEqu(i1) = fixNode(i1);		% Saves Node # to a matrix for use in the Function
+			fixValue = input('Knockout with the Value = ');		% How the node should be modified
+			ICs(fixNode(i1)) = fixValue;		% Replaces the value of Node # to the declared value
 		end
 	else
 	end
@@ -28,87 +37,76 @@ fix = input('How many nodes should be fixed? (0 -> 10) = ');
 %% Ode45 Solver
 % This solves the Diff Equs. Kinda important.
 
-[T,Y] = ode45(@func, [0 1000], ICs );   
+[T,Y] = ode45(@func_v2, [0 250], ICs );   
 
 %% Create Plots
 % This plots Time vs. Organism & Organism vs. Organism
 
 graph = input('\nShould plots be generated? (y/n) = ','s');
 if graph == 'y';
-	xPlot = 4; yPlot = 5;  % Layout of Plots
-	subplot(xPlot,yPlot,1:10);
-		plot(T,Y(:,1),'-b', T,Y(:,2),'-r', T,Y(:,3),'-k', T,Y(:,4),'-g', T,Y(:,5),'-c', T,Y(:,6),'--b', T,Y(:,7),'--r', T,Y(:,8),'--k', T,Y(:,9),'--g', T,Y(:,10),'--c');
-		legend('Org 1', 'Org 2', 'Org 3', 'Org 4', 'Org 5', 'Org 6', 'Org 7', 'Org 8', 'Org 9', 'Org 10');
+	yPlot = 3; xPlot = 4;		% Layout of Plots
+	subplot(yPlot,xPlot,1:4);
+		plot(T,Y(:,1),'-b', T,Y(:,2),'-r', T,Y(:,3),'-k', T,Y(:,4),'-g', T,Y(:,5),'--b', T,Y(:,6),'--r', T,Y(:,7),'--k', T,Y(:,8),'--g');
+		legend('Org 1', 'Org 2', 'Org 3', 'Org 4', 'Org 5', 'Org 6', 'Org 7', 'Org 8');
 		title('Time vs. Organism')
-	subplot(xPlot,yPlot,11);
+	subplot(yPlot,xPlot,5);
 		plot(Y(:,1),Y(:,2),'-b');
 		title('Org 1 vs. Org 2')
-	subplot(xPlot,yPlot,12)
+	subplot(yPlot,xPlot,6)
 		plot(Y(:,2),Y(:,3),'-b')
 		title('Org 2 vs. Org 3')
-	subplot(xPlot,yPlot,13)
+	subplot(yPlot,xPlot,7)
 		plot(Y(:,3),Y(:,4),'-b')
 		title('Org 3 vs. Org 4')
-	subplot(xPlot,yPlot,14)
+	subplot(yPlot,xPlot,8)
 		plot(Y(:,4),Y(:,5),'-b')
 		title('Org 4 vs. Org 5')
-	subplot(xPlot,yPlot,15)
+	subplot(yPlot,xPlot,9)
 		plot(Y(:,5),Y(:,6),'-b')
 		title('Org 5 vs. Org 6')
-	subplot(xPlot,yPlot,16)
+	subplot(yPlot,xPlot,10)
 		plot(Y(:,6),Y(:,7),'-b')
-		title('Org 6 vs. Org 7')
-	subplot(xPlot,yPlot,17)
+		title('Org 6 vs. Org 1')
+	subplot(yPlot,xPlot,11)
 		plot(Y(:,7),Y(:,8),'-b')
 		title('Org 7 vs. Org 8')
-	subplot(xPlot,yPlot,18)
-		plot(Y(:,8),Y(:,9),'-b')
-		title('Org 8 vs. Org 9')
-	subplot(xPlot,yPlot,19)
-		plot(Y(:,9),Y(:,10),'-b')
-		title('Org 9 vs. Org 10')
-	subplot(xPlot,yPlot,20)
-		plot(Y(:,10),Y(:,1),'-b')
-		title('Org 10 vs. Org 1')
-else
-end
-
-%% Save Matrix Query
-% This lets the user save the T & Y Matrix to a Spreadsheet
-
-file = input('\nShould the T vs. Y Matrix be saved to a spreadsheet? (y/n) = ','s');
-if file=='y'
-	fileName = input('Enter in the File Name w/o the .csv extension = ','s');
-	fileExt = '.csv';
-	fileName = strcat(fileName, fileExt);   % Adds .csv to the desired fileName
-	fileMatrix = [T Y];   % Creates a Matrix with the values of T & Y
-	csvwrite(fileName,fileMatrix);
+	subplot(yPlot,xPlot,12)
+		plot(Y(:,8),Y(:,1),'-b')
+		title('Org 8 vs. Org 1')
 else
 end
 
 %% Determine Steady State Values
-% This lets the user determine the Steady State Values
+% This lets the user retrieve the Steady State Values
 
-steady = input('\nShould steady-state values be calculated? (y/n) = ','s');
+steady = input('\nShould steady-state values be printed? (y/n) = ','s');
 if steady=='y'
-	i = 11;   % This confirms that the while loop does not end early
-	steadyNode = input('Which node should determine Steady State? (#) = ');
-	while i <= numel(T);   % While 'i' is less than the Max-Time interval tested
-		if abs(Y(i,steadyNode) - Y(i-10,steadyNode)) <= 0.00005   % This tests for Steady State
-			fprintf('Node %g reached a steady state\n', steadyNode);
-			for j=1:10
-				steadyValue(j,1) = j;   % This makes column 1 be values of 1 to 10
-				steadyValue(j,2) = Y(1000,j);   % This makes column 2 be the equilibrium reached
-				fprintf('Node %g \t %g \n',steadyValue(j,1),steadyValue(j,2));
-			end
-			i = numel(T) + 1;
-		else
-			i = i + 1;
-		end
+	fprintf('The steadyState Values in the order of Node 1 --> 8 are:\n');
+	for i = 1:8
+		ans1(i) = Y(end,i);		% Obtains values from the last row in 'Y'
+		fprintf('%f, ',ans1(i));
 	end
 else
 end
     
+%% Determine Constant & newAlpha values.
+% This lets the user retrieve the Steady State Values
+
+steady = input('\nShould constants & alpha values be printed? (y/n) = ','s');
+if steady=='y'
+	fprintf('The Constant Values in the order of Node 1 --> 8 are:\n');
+	for i = 1:8
+		ans2(i) = constant(1,i);	% Retrieves the 'Constants' matrix from func_v2.m
+		fprintf('%f, ',ans2(i));
+    end
+    fprintf('\n\nThe Alpha Values in the order of Node 1 --> 8 are:\n');
+    for i = 1:8
+		ans3(i) = newAlpha(1,i);	% Retrieves the 'newAlpha' matrix from func_v2.m 
+		fprintf('%f, ',ans3(i));
+	end
+else
+end
+
 %% Finish
 % Achievement Unlocked.
 
